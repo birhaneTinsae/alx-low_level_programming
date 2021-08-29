2b55cf7839faa26fe9ae9f313763e0d39b467fd6
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-29 15:53:28 +0300

    fix 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d %s\n",n,"is negative");
+printf("%d %s\n", n, "is negative");
 }else if (n > 0)
 {
-printf("%d %s\n",n,"is positive");
-}else
+printf("%d %s\n", n, "is positive");
+}
+else
 {
-printf("%d %s\n",n,"is zero");
+printf("%d %s\n", n, "is zero");
 }
 return (0);
 }
