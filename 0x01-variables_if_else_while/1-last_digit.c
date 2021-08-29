dbdaf006616b4b9eaea29fc03f595e9d1d444436
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-29 16:25:15 +0300

    fix 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 lastDigit=n%10;
 if (lastDigit < 6 && lastDigit != 0)
 {
-printf("%s %d %s %s %d %s %d\n","Last digit of", n, "is", "and is less than", 6, "and not", 0);
+printf("%s %d %s %d %s %d %s %d\n","Last digit of", n, "is", lastDigit, "and is less than", 6, "and not", 0);
 }
 else if (lastDigit > 5)
 {
-printf("%s %d %s %s %d","Last digit of", n, "is", "and is greater than", lastDigit);
+printf("%s %d %s %d  %s %d","Last digit of", n, "is",lastDigit, "and is greater than", 5);
 }
 else if (lastDigit == 0)
 {
-printf("%s %d %s %s %d","Last digit of", n, "is", "and is ", lastDigit);
+printf("%s %d %s %d %s %d","Last digit of", n, "is", lastDigit, "and is ", 0);
 }
 return (0);
 }
