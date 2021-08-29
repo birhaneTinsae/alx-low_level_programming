083e39c13e533774e095c6f57b5668ddf460de99
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-29 11:56:10 +0300

    fix 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-fprintf(stderr, "%s\" - %s, %s", "and that piece of art is useful", "Dora Korpar", "2015-10-19");
+fprintf(stderr, "%s\" - %s, %s", "and that piece of art is useful\n", "Dora Korpar", "2015-10-19");
 return (0);
 }
