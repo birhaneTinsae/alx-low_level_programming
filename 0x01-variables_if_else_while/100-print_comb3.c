f46eb244fc4d4e79f671b2ec842a30b36fd55bcf
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-31 19:41:26 +0300

    add 0x02-functions

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i, j;
+
+
+for (i = '0'; i <= '9'; i++) 
+{
+putchar(i);
+// for (j = i + 1; i <= '9'; j++){
+// putchar(j);
+// putchar(',');
+// putchar(' ');
+// }
+}
+
+putchar('\n');
+return (0);
+}
