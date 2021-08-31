4cdca9ca9d882aa1e21cd608db0c06d826f2b2a9
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-31 20:22:29 +0300

    add 1-alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,11 @@
  **/
 
 int _putchar(char c);
+/**
+ * print_alphabet - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ **/
 
 void print_alphabet(void);
