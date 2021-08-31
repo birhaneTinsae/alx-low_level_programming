f44ddca62fa846b5e964933741ea28ba5220d0c1
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-31 23:05:34 +0300

    add print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,7 @@ int print_last_digit(int n)
 int lastDigit;
 if (n < 0)
 {
-lastDigit = (n * -1) % 10;
-_putchar(lastDigit + '0');
-return (lastDigit);
-}
-else if (n==INT_MIN)
-{
-_putchar(8 + '0');
-return (8);
+n = n * -1;
 }
 lastDigit = (n % 10);
 _putchar(lastDigit + '0');
