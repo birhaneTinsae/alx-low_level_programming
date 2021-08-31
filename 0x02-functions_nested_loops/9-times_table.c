738cc94e45a707a4a5d16bb04b6199e54e372c9f
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-31 23:25:00 +0300

    add times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,29 @@
  */
 void times_table(void)
 {
-int i, j, r;
+int i, j, res;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-r = i * j;
-_putchar( r+ '0');
+res = (i * j);
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
 }
+if (res >= 10)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res < 10 && j != 0)
+{
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else
+_putchar((res % 10) + '0');
 _putchar('\n');
 }
 }
