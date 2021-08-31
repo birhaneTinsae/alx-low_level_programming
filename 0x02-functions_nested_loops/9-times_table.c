59f1a9e9cc92d379774ee11556bca24cdea8311a
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-31 23:21:28 +0300

    add times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,18 +2,19 @@
 
 /**
  * times_table - writes the character c to stdout
- * 
- * Return: On success 1.
+ *  Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void times_table(void)
 {
-int i, j;
+int i, j, r;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar(i * j + '0');
+r = i * j;
+_putchar( r+ '0');
 }
+_putchar('\n');
 }
 }
