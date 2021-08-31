bf13a3bcb65d0d6ab9640f01f7fa7ef46f66ec45
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-08-31 22:28:16 +0300

    add _abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include<stdlib.h>
 /**
- * print_sign - writes the character c to stdout
+ * _abs - writes the character c to stdout
  * @n: The character to check
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
