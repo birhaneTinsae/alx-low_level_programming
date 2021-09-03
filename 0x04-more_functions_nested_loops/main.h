b8e94ee576e946d4e63e3e9fb8cf5c45e094ef08
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-09-03 10:17:49 +0300

    add 0-isUpper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,108 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ **/
+
+int _putchar(char c);
+/**
+ * print_alphabet - writes the character c to stdout
+ * 
+ *
+ * Return: On success 1.
+ **/
+
+void print_alphabet(void);
+/**
+ * print_alphabet_x10 - writes the alphabet 10x
+ * 
+ *
+ * Return: On success 1.
+ **/
+void print_alphabet_x10(void);
+
+/**
+ * _islower - check is c is a character
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ **/
+int _islower(int c);
+
+/**
+ * _isalpha - check is c is a character
+ * @c: The character to check
+ *
+ * Return: On success 1.
+ **/
+int _isalpha(int c);
+
+/**
+ * print_sign - check is c is a character
+ * @n: The number to check
+ *
+ * Return: On success 1.
+ **/
+int print_sign(int n);
+
+/**
+ * _abs - check is c is a character
+ * @n: The number to check
+ *
+ * Return: On success 1.
+ **/
+
+int _abs(int);
+
+/**
+ * print_last_digit - check is c is a character
+ * 
+ *
+ * Return: On success 1.
+ **/
+int print_last_digit(int);
+
+
+/**
+ * jack_bauer - check is c is a character
+ * 
+ *
+ * Return: On success 1.
+ **/
+void jack_bauer(void);
+
+/**
+ * times_table - check is c is a character
+ * 
+ *
+ * Return: On success 1.
+ **/
+void times_table(void);
+
+/**
+ * add - check is c is a character
+ * 
+ *
+ * Return: On success 1.
+ **/
+
+int add(int, int);
+
+/**
+ * print_to_98 - check is c is a character
+ * 
+ *
+ * Return: On success 1.
+ **/
+void print_to_98(int n);
+
+/**
+ * _isupper - check is c is a character
+ * @c: The number to check
+ *
+ * Return: On success 1.
+ **/
+int _isupper(int c);
