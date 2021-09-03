80d740899ea7c2647c54e6dee41191d73446aada
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-09-03 10:38:55 +0300

    add 4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,12 +3,12 @@
  * print_most_numbers - check is c is a character
  * Return: On success 1.
  **/
-void g(void)
+void print_most_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-if(i != 2 && i != 4)
+if((i != 2) && (i != 4))
 _putchar(i + '0');
 }
 _putchar('\n');
