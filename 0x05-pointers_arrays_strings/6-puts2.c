318d3b2f9cb341cd957932058f5783a5a170e5f9
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-09-06 22:32:05 +0300

    add 4-print_rev.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,28 @@
 #include"main.h"
 #include <stdio.h>
 /**
+ * _strlen - get string length
+ * @s: string argument
+ *
+ * Return: string size
+ */
+int _strlen(char *s)
+{
+char *p = s;
+while (*p != '\0')
+{
+p++;
+}
+return (p - s);
+}
+/**
  * puts2 - get string length
  * @str: string argument
  */
 void puts2(char *str)
 {
 int i;
-for (i = 0; i < sizeof(str); i+2)
+for (i = 0; i < _strlen(str); i+2)
 {
 puts(str[i]);
 }
