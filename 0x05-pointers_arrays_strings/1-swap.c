c49015a2b45f06ccb4e7e8eeaff2b6590b65a814
Birhane Tinsae <birhane.tinsaa@gmail.com>
2021-09-06 15:26:17 +0300

    add 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * swap_int - swap the arguments
- * @a pointer parameter
- * @b pointer parameter two
+ * @a: pointer parameter
+ * @b: pointer parameter two
  */
 void swap_int(int *a, int *b)
 {
